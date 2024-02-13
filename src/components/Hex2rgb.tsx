086a0812f9147ecc66d1dc2e934f802cd0d52e21
Olya Tatianina <olya.tatianina@gmail.com>
2024-02-13 17:40:10 +0400

    fixed

--- src/components/Hex2rgb.tsx
+++ src/components/Hex2rgb.tsx
@@ -1,42 +1,53 @@
 import { useState } from "react";
 
 const Converter = () => {
-  const [rgb, setRgb] = useState("rgb(153, 33, 255)");
+  const [rgb, setRgb] = useState("rgb(204, 204, 255)");
+  const [, setHex] = useState("#CCCCFF");
 
   const hex2rgb = (hex: string) => {
-    const r = parseInt(hex.slice(1, 3), 16);
-    const g = parseInt(hex.slice(3, 5), 16);
-    const b = parseInt(hex.slice(5, 7), 16);
+    let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
 
-    return Number.isNaN(r) || Number.isNaN(g) || Number.isNaN(b)
-      ? "Ошибка!"
-      : `rgb(${r}, ${g}, ${b})`;
+    return result
+      ? {
+          r: parseInt(result[1], 16),
+          g: parseInt(result[2], 16),
+          b: parseInt(result[3], 16),
+        }
+      : null;
   };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.value.length === 7) {
-      setRgb(hex2rgb(e.target.value));
+    const inputValue = e.target.value;
+    const result = hex2rgb(inputValue);
+
+    if (inputValue.match(/^#[0-9a-f]{6}$/i) && result !== null) {
+      setHex(inputValue);
+      setRgb(`rgb(${result.r}, ${result.g}, ${result.b})`);
+    } else if (inputValue.length === 7) {
+      setHex("");
+      setRgb("Ошибка!");
     }
   };
 
   const bgColour = {
     color: {
-      backgroundColor: rgb !== "Ошибка!" ? rgb : "rgb(233,75,53)",
+      backgroundColor: rgb !== "Ошибка!" ? rgb : "rgb(233, 75, 53)",
     },
   };
 
   return (
-    <>
+    <form>
       <div className="container" style={bgColour.color}>
         <input
           name="input"
           type="text"
           className="hex"
           onChange={handleChange}
+          maxLength={7}
         ></input>
         <div className="rgb">{rgb}</div>
       </div>
-    </>
+    </form>
   );
 };
 
